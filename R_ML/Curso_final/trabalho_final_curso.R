91bb3da8bf4fdc6cd1e8f91fd08872e69e3c7ebd
Michel <michel_henrique_911@hotmai.com>
2019-07-14 14:44:12 -0300

    atividade curso/xgboost

---
+++ R_ML/Curso_final/trabalho_final_curso.R
@@ -0,0 +1,39 @@
+library(e1071)
+library(FSelector)
+
+
+dados_creditos = read.csv(file.choose(),header = TRUE, sep = ';')
+amostras = sample(2, 1000, replace = T, prob = c(0.7,0.3))
+amostras
+dados_treino = dados_creditos[amostras==1,]
+dados_teste = dados_creditos[amostras==2,]
+
+modelo_naive = naiveBayes(CLASSE ~., dados_treino)
+modelo_svm = svm(CLASSE ~., dados_treino)
+modelo_naive_atr = naiveBayes(CLASSE ~ CHEQUEESPECIAL + USO_CREDITO + HISTORICO_CREDITO + BALANCO_ATUAL 
+                              + BALANCO_MEDIO_CREDITO + OUTRASFUNCOES + OUTROSPLANOSPGTO + IDADE
+                              + PROPOSITO + TIPOSBENS + EMPREGADO + LOCAL + EMPREGO + RESIDENCIA + TRABAESTRANGEIRO , dados_treino)
+
+
+previsao_naive = predict(modelo_naive, dados_teste)
+previsao_svm = predict(modelo_svm, dados_teste)
+previsao_naive_atr = predict(modelo_naive_atr, dados_teste)
+
+matriz_confusao_naive = table(dados_teste$CLASSE, previsao_naive)
+matriz_confusao_svm = table(dados_teste$CLASSE, previsao_svm)
+matriz_confusao_naive_atr = table(dados_teste$CLASSE, previsao_naive_atr)
+#taxa de acerto
+taxa_naive = (matriz_confusao_naive[1]+matriz_confusao_naive[4])/sum(matriz_confusao_naive)
+taxa_svm = (matriz_confusao_svm[1]+matriz_confusao_svm[4])/sum(matriz_confusao_svm)
+taxa_naive_atr = (matriz_confusao_naive_atr[1]+matriz_confusao_naive_atr[4])/sum(matriz_confusao_naive_atr)
+
+taxa_naive
+taxa_svm
+taxa_naive_atr
+
+
+atributos_importance = random.forest.importance(CLASSE ~., dados_creditos)
+
+importance_order <- atributos_importance[order(atributos_importance$attr_importance),] 
+importance_order
+
---
+++ R_ML/Xgboost/xgboost_learning.R
@@ -0,0 +1,4 @@
+data(agaricus.train, package='xgboost')
+data(agaricus.test, package='xgboost')
+train <- agaricus.train
+test <- agaricus.test
